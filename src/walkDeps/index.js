5ae1f926da00cba98aa0417532547d9b9b5a0db9
glenn vincent hinks <glenn.hinks@kiswe.com>
2018-03-02 08:02:17 -0500

    circular dependency fix #1

--- src/walkDeps/index.js
+++ src/walkDeps/index.js
@@ -20,7 +20,17 @@ results tree object should be
 }
 */
 // babel-traverse babel-types 'babel-types', version: '^6.9.0'  'babel-traverse', version: '^6.9.0'
+const isCircularDependency = (parent, moduleName) => {
+  if (!parent) return false
+  if (!parent.depends) return false
+  const parentsMatchingDepends = parent.depends.filter(parentDependency => parentDependency.module === moduleName)
+  if (parentsMatchingDepends.length > 0) return true
+  return false
+}
+
 const walker = async (task, cb) => {
+  // if (task.depth > 7) return
+  if (isCircularDependency(task.parent, task.dependency.module)) return cb(null, null, null, null)
   const regDeps = await getRegistryDeps(task.dependency)
   task.results[task.dependency.module] = {}
   if (regDeps) {
@@ -29,13 +39,15 @@ const walker = async (task, cb) => {
       if (depName) depends.push({ module: depName, version: regDeps[depName] })
     })
     if (depends.length > 0) {
-      task.results[task.dependency.module] = { depends }
-      const newDeps = depends.map(dependency => ({ dependency, results: task.results[task.dependency.module], q: task.q }))
-      newDeps.forEach(nd => task.q.push(nd, (e, n, v) => console.log(n, v)))
+      task.results[task.dependency.module] = { depends, depth: task.depth }
+      const newDeps = depends.map(dependency => ({ dependency, results: task.results[task.dependency.module], q: task.q, depth: task.depth + 1, parent: task.results }))
+      newDeps.forEach(nd => task.q.push(nd, (e, n, v, p) => {
+        // console.log(`name = ${n} value =${JSON.stringify(v, null, 2)}, parent=${JSON.stringify(p, null, 2)}`)
+      }))
       // depends.forEach(dependency => task.q.push({ dependency, results: task.results[task.dependency.module], q: task.q }, (e, v) => console.log(v)))
     }
   }
-  cb(null, task.dependency.module, task.results[task.dependency.module])
+  cb(null, task.dependency.module, task.results[task.dependency.module], task.results)
 }
 
 const walkDeps = async (moduleToFind) => {
@@ -43,12 +55,12 @@ const walkDeps = async (moduleToFind) => {
   // get an array of { module, semver }
   const collatedDeps = collate(packageJsonDeps)
   let results = { depends: collatedDeps }
-  const q = async.queue(walker, 1)
+  const q = async.queue(walker, 100)
   // here we are with a lovely list of modules and semvers
   // from which we wish to find our top level package that
   // is including the moduleToFind
   q.drain = () => console.log(`all finished ${JSON.stringify(results, null, 2)}`)
-  collatedDeps.forEach((d) => q.push({ dependency: d, results, q }, (e, n, v) => console.log(n, v)))
+  collatedDeps.forEach((d) => q.push({ dependency: d, results, q, depth: 1, parent: undefined }, (e, n, v) => null))
 }
 
 export default walkDeps
