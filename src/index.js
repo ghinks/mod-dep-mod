d56a862dec70e310c4bc1721e83520621f22a261
glenn vincent hinks <glenn.hinks@kiswe.com>
2018-03-05 19:46:27 -0500

    update version

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import program from 'commander'
 import walk from './walkDeps'
 
 program
-  .version('0.0.2')
+  .version('0.0.3')
   .option('-m, --module [value]', 'find root module that requires this module')
   .parse(process.argv)
 
