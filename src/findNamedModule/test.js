e17c356b45f7c3d79ada7c4be2e3c1c6dd1d1691
glenn vincent hinks <glenn.hinks@kiswe.com>
2018-03-17 06:38:08 -0400

    test coverage

--- src/findNamedModule/test.js
+++ src/findNamedModule/test.js
@@ -2,6 +2,16 @@ import { expect } from 'chai'
 import findNamedModule from './index'
 
 describe('Remove all but named module trees', () => {
+  it('expect an empty array with no dep obj passed', () => {
+    const result = findNamedModule()
+    expect(result).to.be.an('Array')
+    expect(result.length).to.be.equal(0)
+  })
+  it('expect an empty array with no name passed', () => {
+    const result = findNamedModule({})
+    expect(result).to.be.an('Array')
+    expect(result.length).to.be.equal(0)
+  })
   it('expect to get 0 leaf result', () => {
     const modules = {
       moduleName1: {
@@ -14,6 +24,20 @@ describe('Remove all but named module trees', () => {
     const result = findNamedModule(modules, 'moduleZ', null)
     expect(result).to.be.an('Array')
     expect(result.length).to.be.equal(0)
+  })
+  it('expect to get 1 root branch result', () => {
+    const modules = {
+      moduleName1: {
+        moduleName1_1: {
+          moduleX: {}
+        }
+      },
+      moduleName2: {}
+    }
+    const result = findNamedModule(modules, 'moduleName1', null)
+    expect(result).to.be.an('Array')
+    expect(result.length).to.be.equal(1)
+    expect(result[0]).to.be.equal('moduleName1')
   })
   it('expect to get 1 leaf result', () => {
     const modules = {
