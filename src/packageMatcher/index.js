5beae54a9190320f741c92f42817350bdba5753b
Glenn Hinks <ghinks@yahoo.com>
2018-11-29 11:27:37 -0500

    remove all semver tests and just use the range test

--- src/packageMatcher/index.js
+++ src/packageMatcher/index.js
@@ -1,75 +1,4 @@
-import {compare, gte, rcompare, maxSatisfying, validRange} from 'semver'
-
-const getMinor = (versions, testValue) => {
-  const regex = /(\d+)\.([\d]+)\.([\dxX]+)/
-  const testValueMatch = testValue.replace(/x/ig, '0').match(regex)
-  return versions.reduce((acc, cur) => {
-    const versionMatch = cur.match(regex)
-    if (versionMatch[1] === testValueMatch[1] & versionMatch[2] === testValueMatch[2]) {
-      if (!acc) {
-        return cur
-      }
-      const accMatch = acc.match(regex)
-      if (versionMatch[3] > accMatch[3]) {
-        return cur
-      }
-    }
-    return acc
-  }, undefined)
-}
-
-const getMajor = (versions, testValue) => {
-  const regex = /(\d+)\.([\dxX]+)\.([\dxX]+)/
-  const testValueMatch = testValue.replace(/x/ig, '0').match(regex)
-  return versions.reduce((acc, cur) => {
-    const versionMatch = cur.match(regex)
-    if (versionMatch[1] === testValueMatch[1]) {
-      if (versionMatch[2] < testValueMatch[2]) {
-        return acc
-      }
-      if (!acc && versionMatch[2] >= testValueMatch[2]) {
-        return cur
-      }
-      const accMatch = acc.match(regex)
-      if (versionMatch[2] === testValueMatch[2] && versionMatch[3] > accMatch[3]) {
-        return cur
-      }
-      if (versionMatch[2] >= testValueMatch[2] && versionMatch[2] > accMatch[2]) {
-        return cur
-      }
-      if (versionMatch[2] >= testValueMatch[2] && versionMatch[3] > accMatch[3]) {
-        return cur
-      }
-    }
-    return acc
-  }, undefined)
-}
-
-const getExact = (versions, testValue) => {
-  if (versions.includes(testValue)) {
-    return testValue
-  }
-  return undefined
-}
-
-const getSingle = (versions, testValue) => {
-  const regexSingleDigit = /(\^?)(\d+)/
-  const match = testValue.match(regexSingleDigit)
-  if (match && match[2]) return getMajor(versions, `${match[2]}.x.x`)
-}
-
-// TODO npm tool came back with highest 1.x.x, investigate
-const getMax = (versions) => versions.sort(rcompare)[0]
-
-const getHighestMatch = (versions, testValue) => {
-  const ascVersions = versions.sort(compare)
-  const match = testValue.match(/(\D?)(\d+.*)/)
-  for (let i = 0; i < ascVersions.length; i++) {
-    if (gte(ascVersions[i], match[2])) {
-      return ascVersions[i]
-    }
-  }
-}
+import {compare, maxSatisfying, validRange} from 'semver'
 
 const getRange = (versions, testValue) => {
   const descVers = versions.sort(compare)
@@ -77,38 +6,9 @@ const getRange = (versions, testValue) => {
 }
 
 const matcher = (versions, testValue) => {
-  // TODO check versions and length
-
-  // check ranges
-  // eslint-disable-next-line
   if (validRange(testValue)) {
-    const regexRange = /(\d+\.[\dxX]+\.[\dxX]+).*(\d+\.[\dxX]\.[\dxX])/
-    const matchRange = validRange(testValue).match(regexRange)
-    if (matchRange) {
-      return getRange(versions, testValue)
-    }
-  }
-
-  // eslint-disable-next-line
-  const regexMajMin = /^([\^\~\=]?)(\d+\.[\dxX]+\.[\dxX]+)/
-  const match = testValue.match(regexMajMin)
-  if (match && ((match[1] === '~') || (testValue.match(/(\d+\.\d+\.[xX]+)/)))) {
-    return getMinor(versions, match[2])
-  } else if (match && match[1] === '^') {
-    return getMajor(versions, match[2])
-    // eslint-disable-next-line
-  } else if (match && match[1] === '=') {
-    return getExact(versions, match[2])
-  } else if (match && !testValue.match(/([\^~]?)(\d+\.[xX]+\.[xX]+)/) && !testValue.match(/>=.*/)) {
-    return getExact(versions, testValue)
-    // eslint-disable-next-line
-  } else if (testValue.match(/(\^?)(\d+)/) && !testValue.match(/\>\=.*/)) {
-    return getSingle(versions, testValue)
-  } else if (testValue === '*') {
-    return getMax(versions)
-  } else if (testValue.match(/>=.*/)) {
-    return getHighestMatch(versions, testValue)
+    return getRange(versions, testValue)
   }
 }
 
-export { matcher as default, getMinor, getMajor, getExact, getSingle, getHighestMatch, getMax, getRange }
+export { matcher as default, getRange }
--- src/packageMatcher/test.js
+++ src/packageMatcher/test.js
@@ -1,104 +1,9 @@
 import {expect} from 'chai'
-import matcher, {getMinor, getMajor, getExact, getSingle, getMax, getRange } from './index'
+import matcher from './index'
 import versions from './testData/versions'
-import increasingVersions from './testData/increasingVersions'
 
 describe('package matching', () => {
   const reversedVersions = [...versions].reverse()
-  describe('minor', () => {
-    it('expect minor match 1.0.9', () => {
-      const result = getMinor(reversedVersions, '1.0.0')
-      expect(result).to.be.equal('1.0.9')
-    })
-    it('expect minor match 1.0.2', () => {
-      const result = getMinor(['1.0.0', '1.0.1', '1.0.2'], '1.0.9')
-      expect(result).to.be.equal('1.0.2')
-    })
-  })
-  describe('major', () => {
-    it('expect major match 1.2.9', () => {
-      const result = getMajor(versions, '1.2.0')
-      expect(result).to.be.equal('1.2.9')
-    })
-    it('expect major match 7.2.1', () => {
-      const result = getMajor(versions, '7.2.1')
-      expect(result).to.be.equal('7.2.1')
-    })
-    it('expect major match 2.14.1', () => {
-      const result = getMajor(increasingVersions, '2.14.1')
-      expect(result).to.be.equal('2.14.1')
-    })
-  })
-  describe('single digit', () => {
-    it('expect single digit match 1', () => {
-      const result = getSingle(versions, '1')
-      expect(result).to.be.equal('1.2.9')
-    })
-    it('expect single digit match ^1', () => {
-      const result = getSingle(versions, '^1')
-      expect(result).to.be.equal('1.2.9')
-    })
-    it('expect single digit match 7', () => {
-      const result = getSingle(versions, '7')
-      expect(result).to.be.equal('7.2.1')
-    })
-    it('expect no single digit match', () => {
-      const result = getSingle([], 'x')
-      expect(result).to.be.undefined // eslint-disable-line
-    })
-  })
-  describe('exact', () => {
-    it('expect no match on 100.100.100', () => {
-      const result = getExact(versions, '100.100.100')
-      expect(result).to.be.an('undefined')
-    })
-    it('expect exact match 1.0.1', () => {
-      const result = getExact(versions, '1.0.1')
-      expect(result).to.be.equal('1.0.1')
-    })
-    it('expect exact match 1.2.1', () => {
-      const result = getExact(versions, '1.2.1')
-      expect(result).to.be.equal('1.2.1')
-    })
-    it('expect exact match 7.2.1', () => {
-      const result = getExact(versions, '7.2.1')
-      expect(result).to.be.equal('7.2.1')
-    })
-  })
-  describe('max (*)', () => {
-    it('expect max version *', () => {
-      const result = getMax(versions, '*')
-      expect(result).to.be.equal('7.2.1')
-    })
-  })
-  describe('range x < y', () => {
-    // 6.0.0 <=6.1.1
-    // 2.2.7 <3
-    // 0.3.0 < 0.4.0
-    // 1.0.33-1 <1.1.0-0
-    it('expect 0.0.1 < 0.1.0 to match 0.0.1', () => {
-      const result = getRange(versions, '0.0.1 < 0.1.0')
-      expect(result).to.be.equal('0.0.1')
-    })
-    it('expect 0.0.1 <0.1.0 to match 0.0.1', () => {
-      const result = getRange(versions, '0.0.1 <0.1.0')
-      expect(result).to.be.equal('0.0.1')
-    })
-    it('expect 1.0.0 <=1.0.9', () => {
-      const result = getRange(versions, '1.0.0 <=1.0.9')
-      expect(result).to.be.equal('1.0.0')
-    })
-    it('expect 1.0.0 < 3', () => {
-      const result = getRange(versions, '1.0.0 < 3')
-      expect(result).to.be.equal('1.0.0')
-    })
-  })
-  describe('equality = ', () => {
-    it('expect =1.0.0 to match 1.0.0', () => {
-      const result = matcher(versions, '=1.0.0')
-      expect(result).to.be.equal('1.0.0')
-    })
-  })
   describe('matcher tests', () => {
     describe('Use incrementing versions', () => {
       it('Expect to match 1.1.5 exactly', () => {
@@ -147,7 +52,7 @@ describe('package matching', () => {
       })
       it('expect highest version >=2.0.0', () => {
         const result = matcher(versions, '>=2.0.0')
-        expect(result).to.be.equal('2.0.1')
+        expect(result).to.be.equal('7.2.1')
       })
       it('expect highest version 1.1.x', () => {
         const result = matcher(versions, '1.1.x')
@@ -160,6 +65,14 @@ describe('package matching', () => {
       it('expect to match range 1.0.0 < 3', () => {
         const result = matcher(versions, '1.0.0 < 3')
         expect(result).to.be.equal('1.0.0')
+      })
+      it('expect a match from ^0.3.0', () => {
+        const result = matcher(versions, '^0.3.0')
+        expect(result).to.be.equal('0.3.1')
+      })
+      it('expect a match from ~0.3.0', () => {
+        const result = matcher(versions, '~0.3.0')
+        expect(result).to.be.equal('0.3.1')
       })
     })
     describe('Use decrementing versions', () => {
